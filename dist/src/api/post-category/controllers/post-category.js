ff49e79c17833a339353caa3a41ec04b7b14b1de
abdillah-me <muhammad.abdillah@idstar.group>
2024-12-30 02:55:55 +0700

    first

---
+++ dist/src/api/post-category/controllers/post-category.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * post-category controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::post-category.post-category');
---
+++ dist/src/api/post-category/services/post-category.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * post-category service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::post-category.post-category');
